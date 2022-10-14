cf12a303e5b34491640c51d48f2d59426286a77d
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-14 15:32:07 -0700

    Use puts to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  *main - Entry point
+  *Output - Programming is like building a multilingual puzzle
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	/*declarations*/
+	char inp1[60];
+
+	strcpy(inp1, "\"Programming is like building a multilingual puzzle");
+	puts(inp1);
+	return (0);
+}
