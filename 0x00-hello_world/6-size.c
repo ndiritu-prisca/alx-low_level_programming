c685ae7146eda148d5c8b293f6209083dd05f639
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-14 16:31:30 -0700

    Print the various sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Output sizes
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	/*Declaration*/
+	char charVar;
+	int intVar;
+	long int longVar;
+	long long int long2Var;
+	float floatVar;
+
+	/*Statements*/
+	printf("Size of a char: %zu bytes\n", sizeof(charVar));
+	printf("Size of an int: %zu bytes\n", sizeof(intVar));
+	printf("Size of a long: %zu bytes\n", sizeof(longVar));
+	printf("Size of a long long: %zu bytes\n", sizeof(long2Var));
+	printf("Size of a float: %zu bytes\n", sizeof(floatVar));
+	return (0);
+}
