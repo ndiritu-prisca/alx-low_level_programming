3a2d6c85c05bd50ec63f8b7fa68d946ff7beb489
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-17 05:26:34 -0700

    Use correct while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,12 +13,13 @@
 
 int main(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
 			putchar(ch);
+		ch++;
 	}
 
 	putchar('\n');
