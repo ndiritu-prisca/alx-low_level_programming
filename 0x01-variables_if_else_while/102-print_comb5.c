ae466a85791ee309c325a06031c7a72ec31916e4
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-18 04:16:32 -0700

    2 2-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/*Prints all possible combinations of single-digit numbers*/
+
+/**
+  *main -Entry
+  *
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	a = 48;
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 57)
+		{
+			c = a;
+			while (c < 58)
+			{
+				d = b + 1;
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (a < '9' || b < '8' || c < '9' || d < '9')
+					{
+						putchar(44);
+						putchar(32);
+					}
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
