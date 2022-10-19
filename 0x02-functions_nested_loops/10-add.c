7e839c0bc4bea4bfeb51b27a844c004c5194f19e
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-19 16:08:31 -0700

    Sum of 2 digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *add - a function that adds two integers and returns the result.
+  *
+  *@int1: first integer
+  *@int2: second integer
+  *
+  *Return: int1 + int2
+  */
+
+int add(int1, int2)
+{
+	return (int1 + int2);
+}
