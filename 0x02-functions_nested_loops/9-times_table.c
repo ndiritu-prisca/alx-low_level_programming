c9bfdf216a622c63cf0a90bc5c3d78881a20137e
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-19 15:59:09 -0700

    cAREFUL EDITING

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,6 +28,14 @@ void times_table(void)
 			{
 				_putchar(' ');
 			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
 		}
 		_putchar('\n');
 	}
