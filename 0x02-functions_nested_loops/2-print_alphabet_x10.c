bd121827a88e318694d2cabe3d60ccd716080ab9
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-19 13:37:12 -0700

    10 TIMES

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 -prints alphabets 10 times
+  *
+  *Return: 0
+  */
+
+void print_alphabet_x10(void)
+{
+	char c, c1;
+
+	c1 = '0';
+	while (c1 <= '9')
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		c1++;
+		_putchar('\n')
+	}
+}
