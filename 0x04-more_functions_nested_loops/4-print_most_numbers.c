c17c32e9a5aa77f42fdb8f47050f18c0f9480cae
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 16:41:55 -0700

    Add ''

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 {
 	char c;
 
-	c = 0;
-	while (c <= 9)
+	for ( c = '0'; c <= '9'; c++)
 	{
-		if ((c != 2) || (c != 4))
+		if ((c != '2') || (c != '4'))
 		{
 			_putchar(c);
 		}
