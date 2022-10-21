d0f1946ecce219ff62bb23f91b74982149c1903d
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 15:12:09 -0700

    More than 80 chars

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
-  *print_most_numbers - function that prints the numbers, from 0 to 9, followed by a
-  *new line exce[t 2 and 4
+  *print_most_numbers - function that prints the numbers, from 0 to 9
+  *followed by a new line except 2 and 4
   *
   *Return: void
   */
 
 void print_most_numbers(void)
 {
-	char c;
+	char c = 0;
 
-	for (c = 0; c <= 9; c++)
+	while (c <= 9)
 	{
 		if (c != 2 && c != 4)
 		{
 			continue;
 			_putchar(c + 48);
 		}
+		c++;
 	}
 	_putchar('\n');
 }
