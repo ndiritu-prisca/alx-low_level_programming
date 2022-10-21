4cafc530730205a069fda401c56b6cbf242d1f7e
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 15:03:45 -0700

    Correct function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-  *print_numbers - function that prints the numbers, from 0 to 9, followed by a
-  *new line.
+  *print_most_numbers - function that prints the numbers, from 0 to 9, followed by a
+  *new line exce[t 2 and 4
   *
   *Return: void
   */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
