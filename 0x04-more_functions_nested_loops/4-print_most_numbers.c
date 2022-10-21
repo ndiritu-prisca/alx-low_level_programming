fd363b026534d4595d5393bbe66981a0d3af566d
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 15:49:19 -0700

    Print 0 to 14 10 times

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
   *print_most_numbers - function that prints the numbers, from 0 to 9
   *followed by a new line except 2 and 4
   *
-  *Return: void
+  *Return: numbers other than 2 and 4
   */
 
 void print_most_numbers(void)
@@ -20,6 +20,6 @@ void print_most_numbers(void)
 			_putchar(c + 48);
 		}
 		c++;
+		_putchar('\n')
 	}
-	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *more_numbers - a function that prints 10 times the numbers
+  *from 0 to 14 followed by a new line
+  *
+  *Return: *10 a-z
+  */
+
+void more_numbers(void)
+{
+	char c, cm;
+
+	cm = 0;
+	while (cm < 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + '0');
+		}
+		cm++;
+		_putchar('\n');
+	}
+}
