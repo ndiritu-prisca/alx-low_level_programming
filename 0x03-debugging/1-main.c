cbaf8dd78c3bb74767685caf0d735a75c43157d7
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 05:46:12 -0700

    Use /**

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-	int i; /*don't delete it!*/
+	int i;
 
 	printf("Infinite loop incoming :(\n");
-/*don't delete it!*/
-	i = 0; /*don't delete it!*/
-/*don't delete it!*/
-	while (i < 10) /*don't delete it!*/
-	{ /*don't delete it!*/
-		putchar(i); /*don't delete it!*/
-	} /*don't delete it!*/
-/*don't delete it!*/
+	/**
+	*i = 0;
+	*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
