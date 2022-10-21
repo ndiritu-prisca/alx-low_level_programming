ed502d8b37de5a16991f781dedc2962230521721
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 05:24:30 -0700

    Added space

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	int i;/*don't delete it!*/
+	int i; /*don't delete it!*/
 
 	printf("Infinite loop incoming :(\n");
 /*don't delete it!*/
-	i = 0;/*don't delete it!*/
+	i = 0; /*don't delete it!*/
 /*don't delete it!*/
-	while (i < 10)/*don't delete it!*/
-	{/*don't delete it!*/
-		putchar(i);/*don't delete it!*/
-	}/*don't delete it!*/
+	while (i < 10) /*don't delete it!*/
+	{ /*don't delete it!*/
+		putchar(i); /*don't delete it!*/
+	} /*don't delete it!*/
 /*don't delete it!*/
 	printf("Infinite loop avoided! \\o/\n");
 
