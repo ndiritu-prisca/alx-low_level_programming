854e23f7ae23faa16cf2ab4571cbdaac7220bade
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 06:56:52 -0700

    Slight changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int a, b, c, largest;
 
-	if (a > b && a > c && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (a > b && a > c && b < c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c && a > c)
-	{
-		largest = b;
-	}
-	else if (b > a && b > c && a < c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
