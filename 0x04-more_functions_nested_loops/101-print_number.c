aae9a206746bf6abd8b41350dde6f0e42c8b3373
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-21 18:56:24 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+  *print_number - a function that prints an integer.
+  *@n: parameter to be checked
+  *Return: void
+  */
+
+void print_number(int n)
+{
+	int x, y, digits, digit, to_power;
+	unsigned int t, n2, num;
+
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		t = -n;
+	}
+	else
+	{
+		t = n;
+	}
+
+	num = t;
+
+	while (num >= 10)
+	{
+		num = num / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	to_power = 1;
+	x = 1;
+
+	while (i < digits)
+	{
+		to_power = to_power * 10;
+		i++;
+	}
+	y = power;
+	while (y >= 1)
+	{
+		n2 = (t / y) % 10;
+		_putchar(n2 + '0');
+		y = y / 10;
+	}
+}
