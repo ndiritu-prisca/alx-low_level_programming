cc7eff51e235b2870934e23b207805e36a6e965e
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-25 10:09:50 -0700

    Rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@
 
 void rev_string(char *s)
 {
-	int len = 0;
+	int len, temp, i;
 
-	while (len)
-		_putchar(s[++len]);
-	while (s[len] != '\0')
+	while (s[i++])
 		len++;
-	while (len)
-		_putchar(s[--len]);
-	_putchar('\n');
+	for (i = len - 1; i >= len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
