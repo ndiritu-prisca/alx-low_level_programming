6b94a2921f650207f59df2bbb1f8330b7ccdd2a0
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-26 03:11:30 -0700

    Remove * before _strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  **_strcpy - a function that copies the string pointed to by src
+  *_strcpy - a function that copies the string pointed to by src
   *including the terminating null byte (\0)
   *to the buffer pointed to by dest.
   *@dest: parameter to be checked
@@ -15,6 +15,5 @@ char *_strcpy(char *dest, char *src)
 
 	while (*src)
 		*dest++ = *src++;
-
 	return (temp);
 }
