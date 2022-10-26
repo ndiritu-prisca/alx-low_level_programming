357ef4a88fb750594f83b9f1a811d609fb2f99ca
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-26 00:22:11 -0700

    Introduce a second int

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len = 0;
 
-	while (str[i] != 0)
+	while (str[len] != '\0')
+		len++;
+
+	len = len - 1;
+	for (; i <= len; i + 2)
 	{
 		_putchar(str[i]);
-		i + 2;
 	}
 	_putchar('\n');
 }
