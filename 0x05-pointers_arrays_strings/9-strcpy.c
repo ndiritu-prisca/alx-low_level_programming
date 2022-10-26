0548d7a3ee073063a80871d449e5902eeb9bd77b
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-26 04:19:45 -0700

    Alternative

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len;
+	int len = 0;
 
-	while (src[len] <= '\0')
+	while (*src <= '\0')
 	{
-		src[len];
+		src++;
 		len++;
 	}
 	dest[len] = src[len];
