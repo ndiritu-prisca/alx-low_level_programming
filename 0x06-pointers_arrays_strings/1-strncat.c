fe75e7e2e06934f5bfa1df4d56a93aa99bd85bca
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-27 15:07:48 -0700

    Add !=

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,8 +17,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[len] != '\0')
 		len++;
-	for (i = 0; i < n; i++, len++)
+	for (i = 0; i < n && src[i] != '\0'; i++, len++)
 		dest[len] = src[i];
 
+	dest[len] = '\0';
 	return (dest);
 }
