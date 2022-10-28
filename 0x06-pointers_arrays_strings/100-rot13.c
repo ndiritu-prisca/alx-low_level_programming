83fbd0cf7ed90e4a7671a454c554f22d359aa256
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-28 16:02:40 -0700

    Use while and for

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,21 +12,19 @@ char *rot13(char *str)
 
 	while (str[i] != '\0')
 	{
-		while ((str[i] >= 'a' && str[i] <= 'z') ||
-				(str[i] >= 'A' && str[i] <= 'Z'))
+		if ((str[i] >= 'a' && str[i] <= 'm') ||
+				(str[i] >= 'A' && str[i] <= 'M'))
 		{
-			if ((str[i] >= 'a' && str[i] <= 'm') ||
-					(str[i] >= 'A' && str[i] <= 'M'))
-			{
-				str[i] = str[i] + 13;
-			}
-			else
-			{
-				str[i] = str[i] - 13;
-			}
-			i++;
+			str[i] = str[i] + 13;
+		}
+		else if ((str[i] >= 'n' && str[i] <= 'z') ||
+				(str[i] >= 'N' && str[i] <= 'Z'))
+		{
+			str[i] = str[i] - 13;
 		}
 		i++;
 	}
-	return (str);
+	i++;
+
+return (str);
 }
