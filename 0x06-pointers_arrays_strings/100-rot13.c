78884b91edeb2ad87f486cbc124f3c25fc9cb761
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-28 16:06:25 -0700

    If and else if

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,19 +10,15 @@ char *rot13(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	if ((str[i] >= 'a' && str[i] <= 'm') ||
+			(str[i] >= 'A' && str[i] <= 'M'))
 	{
-		if ((str[i] >= 'a' && str[i] <= 'm') ||
-				(str[i] >= 'A' && str[i] <= 'M'))
-		{
-			str[i] = str[i] + 13;
-		}
-		else if ((str[i] >= 'n' && str[i] <= 'z') ||
-				(str[i] >= 'N' && str[i] <= 'Z'))
-		{
-			str[i] = str[i] - 13;
-		}
-		i++;
+		str[i] = str[i] + 13;
+	}
+	else if ((str[i] >= 'n' && str[i] <= 'z') ||
+			(str[i] >= 'N' && str[i] <= 'Z'))
+	{
+		str[i] = str[i] - 13;
 	}
 	i++;
 
