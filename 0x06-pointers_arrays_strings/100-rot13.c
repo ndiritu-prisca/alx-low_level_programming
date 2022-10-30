5e77fbd5c3488ffd8065ae373a59deb40a7c9102
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-30 00:23:20 -0700

    Correct?

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,21 @@
 char *rot13(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
-		while ((str[i] >= 'a' && str[i] <= 'm') ||
-				(str[i] >= 'A' && str[i] <= 'M'))	
-			str[i] = str[i] + 13;
-		if ((str[i] >= 'n' && str[i] <= 'z') ||
-				(str[i] >= 'N' && str[i] <= 'Z'))
-			str[i] = str[i] - 13;
+		while ((str[i] >= 'a' && str[i] <= 'z') ||
+				(str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if ((str[i] >= 'a' && str[i] <= 'm') ||
+					(str[i] >= 'A' && str[i] <= 'M'))
+				str[i] = str[i] + 13;
+
+			else
+				str[i] = str[i - 13];
+
+			i++;
+		}
 		i++;
 	}
 	return (str);
