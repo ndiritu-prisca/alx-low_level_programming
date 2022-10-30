162149c304996037432ec0db060ec745f8711665
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-29 21:48:02 -0700

    Print numbers

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *main - Entry
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	print_number(98);
+	print_number(402);
+	print_number(1024);
+	print_number(0);
+	print_number(-98);
+	return (0);
+}
