1bbc1ecee5d486a81ed7258565522b6381725f57
ndiritu-prisca <priscandiritu@outlook.com>
2022-10-31 12:27:22 -0700

    Version 4

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,18 +12,13 @@ char *rot13(char *str)
 
 	while (str[i] != '\0')
 	{
-		while ((str[i] >= 'a' && str[i] <= 'z') ||
-				(str[i] >= 'A' && str[i] <= 'Z'))
-		{
-			if ((str[i] >= 'a' && str[i] <= 'm') ||
-					(str[i] >= 'A' && str[i] <= 'M'))
-				str[i] = str[i] + 13;	
-
-			else
+		if ((str[i] >= 'a' && str[i] <= 'm') ||
+				(str[i] >= 'A' && str[i] <= 'M'))
+			str[i] = str[i] + 13;
+		else
+			while ((str[i] >= 'n' && str[i] <= 'z') ||
+					(str[i] >= 'N' && str[i] <= 'Z'))
 				str[i] = str[i] - 13;
-
-			i++;
-		}
 		i++;
 	}
 	return (str);
