51c5571765eaced0884df7a1619c8c90221cc2f4
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-09 13:48:45 -0800

    Args to string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * argstostr -a function that concatenates all the arguments
+  * of your program.
+  * @ac: argument count
+  * @av: pointer to 2 dimensional array of strings
+  * Return: pointer to concatenated string
+  */
+char *argstostr(int ac, char **av)
+{
+	int i, j, len;
+	int k = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+	len++;
+
+	p = malloc(sizeof(*p) * len);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[k] = av[i][j];
+			k++;
+		}
+		p[k] = '\n';
+		k++;
+	}
+	return (p);
+}
