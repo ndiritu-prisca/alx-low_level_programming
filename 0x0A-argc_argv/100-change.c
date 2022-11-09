31259b61f7052356213c9446b20dbcd2378cf39a
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-09 01:09:18 -0800

    Reduce lines

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
   * main -entry
   * @argc: argument count
@@ -9,46 +8,35 @@
   */
 int main(int argc, char *argv[])
 {
-	int cents = 0;
-	int coins = 0;
+	int cents, coins;
 
-	if (argc == 2)
-	{
-		cents = atoi(*(argv + 1));
-		while (cents > 0)
-		{
-			if (cents % 25 < cents)
-			{
-				cents = cents - 25;
-				coins++;
-			}
-			else if (cents % 10 < cents)
-			{
-				cents = cents - 10;
-				coins++;
-			}
-			else if (cents % 5 < cents)
-			{
-				cents = cents - 5;
-				coins++;
-			}
-			else if (cents % 2 < cents)
-			{
-				cents = cents - 2;
-				coins++;
-			}
-			else if (cents % 1 < cents)
-			{
-				cents = cents - 1;
-				coins++;
-			}
-		}
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", coins);
+	else
+	{
+		cents = atoi(*(argv + 1));
+		if (cents <= 0)
+			printf("0\n");
+		else
+		{
+			for (coins = 0; cents > 0; coins++)
+			{
+				if (cents % 25 >= 0)
+					cents -= 25;
+				else if (cents % 10 >= 0)
+					cents -= 10;
+				else if (cents % 5 >= 0)
+					cents -= 5;
+				else if (cents % 2 >= 0)
+					cents -= 5;
+				else if (cents % 1 >= 0)
+					cents -= 1;
+			}
+			printf("%d\n", coins);
+		}
+	}
 	return (0);
 }
