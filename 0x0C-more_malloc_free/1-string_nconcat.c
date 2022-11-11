9db000e9080e2f0aecbc86a9a2fdd05dd061a041
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-11 11:51:11 -0800

    Include len1 for i

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i <= len1; i++)
 		p[i] = s1[i];
 	for (j = 0; j <= n; j++, i++)
 		p[i] = s2[j];
