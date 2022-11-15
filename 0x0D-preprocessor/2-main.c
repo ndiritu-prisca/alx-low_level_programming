450efa043c6a5d392f024341277fc114cf48d10c
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-15 06:01:56 -0800

    Chec betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,8 @@
 
 /**
   * main - entry
-  * a program that prints the name of the file it was compiled from, followed by
-  * a new line.
+  * a program that prints the name of the file it was compiled from,
+  * followed by a new line.
   * Return: 0
   */
 int main(void)
