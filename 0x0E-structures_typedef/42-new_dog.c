9d124f8ca85e76291b5ac3a5efc1057f2508b149
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-15 13:29:11 -0800

    4

--- 0x0E-structures_typedef/42-new_dog.c
+++ 0x0E-structures_typedef/42-new_dog.c
@@ -2,27 +2,6 @@
 #include <stdlib.h>
 
 /**
-  * _strcpy - a function that copies a string src to dest
-  * @dest:where string will be copied to
-  * @src: string to be coiped
-  * Return: pointer to new string
-  */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-	int len = 0;
-
-	while (src[len] != '\0')
-		len++;
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
   * new_dog - a function that creates a new dog.
   * @name: name of dog
   * @age: age of dog
@@ -34,32 +13,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ndog;
 	char *new_name;
 	char *new_owner;
-	int len1, len2;
+	int len1, len2, i;
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
-	{
 		return (NULL);
-	}
+	
 	for (len1 = 1; *name != '\0'; len1++, name++)
 		;
-	new_name = malloc(sizeof(*new_name) * len1);
+	new_name = malloc(sizeof(*name) * len1);
 	if (new_name == NULL)
 	{
 		free(ndog);
 		return (NULL);
 	}
+	for (i = 0;  *name != '\0'; i++, name++)
+		new_name[i] =  *name;
+	new_name[i] = '\0';
+
 	for (len2 = 1; *owner != '\0'; len2++, owner++)
 		;
-	new_owner = malloc(sizeof(*new_owner) * len2);
+	new_owner = malloc(sizeof(*owner) * len2);
 	if (new_owner == NULL)
 	{
 		free(new_name);
 		free(ndog);
 		return (NULL);
 	}
-	_strcpy(new_name, name);
-	_strcpy(new_owner, owner);
+	for (i = 0; *owner != '\0'; i++, owner++)
+		new_owner[i] = *owner;
+	new_owner[i] = '\0';
+
 	ndog->name = new_name;
 	ndog->age = age;
 	ndog->owner = new_owner;
