2228b00247c929f8bdbd1f70edc868c1f51bdf0c
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-01 02:04:02 -0800

    Reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - a function that reverses a listint_t linked list.
+  * @head: pointer to first pointer
+  * Return: pointer to the first node of the reversed list
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *temp2;
+
+	temp = *head;
+	if (*head == NULL)
+		return (NULL);
+	temp2 = temp->next;
+	temp->next = NULL;
+
+	while (temp2 != NULL)
+	{
+		temp = temp2;
+		temp2 = temp2->next;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/test/101-main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * main - entry
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+
+	head2 = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint_safe(head2);
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint_safe(head);
+	return (0);
+}
