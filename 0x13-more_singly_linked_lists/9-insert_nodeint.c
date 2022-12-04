1ccbffbc5bf4397d2dc5fc84038a9635815b62f9
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-04 06:13:23 -0800

    Changes to insert at nth node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,13 +29,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
-	for (i = 0; i < (idx - 1); i++)
+	for (i = 0; i < idx; i++)
 	{
+		if (i == (idx - 1))
+		{
+
+			new->n = n;
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
 		temp = temp->next;
 	}
-	new->n = n;
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new);
+	return (NULL);
 }
