58c45fba7067f20ca5f3a7b2404af23b5e2a249d
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-04 06:17:51 -0800

    Remove *head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if ((*head == NULL) && (idx == 0))
+	if (idx == 0)
 	{
 		new->n = n;
 		new->next = *head;
