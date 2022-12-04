a965143309f34c03e618192ea6628fce936237eb
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-04 05:53:04 -0800

    initialize i to 0

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,12 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	while (head)
+	while (head != NULL)
 	{
 		if (i == index)
 			return (head);
