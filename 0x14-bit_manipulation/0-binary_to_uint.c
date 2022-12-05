04456e86766bf4af9d0bdd095a896cc1714e034b
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-05 13:52:45 -0800

    Version 3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+  * power - a function that gets value of base raised to a power
+  * @base: base
+  * @raise: number to raise to
+  * Return: result
+  */
+unsigned int power(unsigned int base, unsigned int raise)
+{
+	int res = 1;
+
+	for (; raise > 0; raise--)
+		res = res * base;
+	return (res);
+}
+
+/**
   * binary_to_uint -  a function that converts a binary number to an unsigned
   * int.
   * @b: poiter to a string
@@ -9,25 +24,32 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int p = 0;
+	unsigned int p, i = 0;
+	unsigned int value;
+	unsigned int num;
+	unsigned int sum = 0;
 	const char *s = b;
 
 	if (*b == '\0')
 		return (0);
 	while (*b != '\0')
 	{
-		if (*b == '0' || *b == '1')
-			b++;
+		b++;
+		i++;
+	}
+	b--;
+	i--;
+
+	for (p = 0; s[p] != '\0'; p++, i--)
+	{
+		value = s[p] - '0';
+		if ((value == 0) || (value == 1))
+		{
+			num = power(2, i);
+			sum = sum + (value * num);
+		}
 		else
 			return (0);
 	}
-	b--;
-	while (s <= b)
-	{
-		p = p << 1;
-		if (*s == '1')
-			p = p + 1;
-		s++;
-	}
-	return (p);
+	return (sum);
 }
