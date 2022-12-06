473440323fbe06cd762f3405062b161eaa13323a
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-06 12:58:24 -0800

    Use NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
   * binary_to_uint -  a function that converts a binary number to an unsigned
@@ -12,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int p = 0;
 	const char *s = b;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 	while (*b != '\0')
 	{
