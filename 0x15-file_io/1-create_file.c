e49ee1fb57e7fa9026450b1121f45e242f25cfbd
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-11 02:55:42 -0800

    Updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (count = 0; *text_content != '\0'; count++)
-			;
-		w = write(fd, str, count);
+		for (count = 0; *str != '\0'; count++)
+			str++;
+		w = write(fd, text_content, count);
 		if (w == -1)
 			return (-1);
 	}
