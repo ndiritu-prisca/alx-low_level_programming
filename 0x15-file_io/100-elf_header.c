2700c5b71c2c78860e810bd6bb17416974bde35f
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-15 03:40:53 -0800

    Update on type

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -215,9 +215,9 @@ void printEntry(unsigned long int e_entry, unsigned char *e_ident)
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)e_entry);
+		printf("%x\n", (unsigned int)e_entry);
 	else
-		printf("%#lx\n", e_entry);
+		printf("%lx\n", e_entry);
 }
 
 /**
